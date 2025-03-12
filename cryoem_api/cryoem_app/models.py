85b10f4b38faad8a1649f3d13ac6b94425636043
Elena Cores <elenayunxin@gmail.com>
2025-03-12 19:15:40 +0100

    model ctf defocusu y defocusv

--- cryoem_api/cryoem_app/models.py
+++ cryoem_api/cryoem_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CTF(models.Model):
+    defocusu = models.FloatField()
+    defocusv = models.FloatField()
+
+    def __str__(self):
+        return f"CTF(defocusu={self.defocusu}, defocusv={self.defocusv})"
+    

83c024bd223751064fd184f5542abb6cd772602b
unknown <elenayunxin@gmail.com>
2025-05-16 18:02:33 +0200

    automatic endpoints swagger

--- cryoem_api/cryoem_api/urls.py
+++ cryoem_api/cryoem_api/urls.py
@@ -16,8 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CryoEM API",
+        default_version='v1',
+        description="API para análisis de micrografías",
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('cryoem_app.urls'))
+    path('', include('cryoem_app.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
+
